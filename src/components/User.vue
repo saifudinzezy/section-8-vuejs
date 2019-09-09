b5864f5e91c821d31bef907b9b06fc4f9c4ffd2e
saifudin <saifudin.zezy7@gmail.com>
2019-09-09 15:03:23 +0700

    komunikasi callback funtion child to parent

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,50 +1,59 @@
 <template>
-    <div class="component">
-        <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
-        <button @click="changeName">Change my Name</button>
-        <p>Name is {{ name }}</p>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <!-- name pertama dari child => UserDatail.vue -->
-                <!-- name kedua dari parent => User.vue -->
-                <!-- nama fungsi dari child = UserDetail.vue @nameWasReset="variabel_parent(User.vue) = variabel_child(UserDetail.vue)" -->
-                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
-            </div>
-        </div>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <p>Name is {{ name }}</p>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <!-- name pertama dari child => UserDatail.vue -->
+        <!-- name kedua dari parent => User.vue -->
+        <!-- nama fungsi dari child = UserDetail.vue @nameWasReset="variabel_parent(User.vue) = variabel_child(UserDetail.vue)" -->
+        <!-- nama fungsi resetFn="nama_fungsi" utk child => UserDetail.vue -->
+        <app-user-detail 
+            :myName="name"
+            @nameWasReset="name = $event"
+            :resetFn="resetName">
+        </app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
 
-    export default {
-        data() {
-            return {
-                //variabel name
-                name: 'Max'
-            }
-        },
-        methods: {
-            changeName(){
-                //ubah nilai name
-                this.name = "Anna"; //jgn lupa titik koma
-            }
-        },
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+export default {
+  data() {
+    return {
+      //variabel name
+      name: "Max"
+    };
+  },
+  methods: {
+    changeName() {
+      //ubah nilai name
+      this.name = "Anna"; //jgn lupa titik koma
+    },
+    //reset name untk komunikasi ke child => UserDetail.vue
+    resetName() {
+      this.name = "Max";
     }
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightblue;
-    }
+div {
+  background-color: lightblue;
+}
 </style>
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,39 +1,50 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-        <!-- pangil key utk mengubah nilai name yg nantinya akan dipanggil oleh parent -->
-        <p>User name: {{ switchName() }}</p>
-        <button @click="resetName">Reset Name</button>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <!-- pangil key utk mengubah nilai name yg nantinya akan dipanggil oleh parent -->
+    <p>User name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
+    <!-- pangil nama fungsi yg sdh di ambil parent ditampung di props -->
+    <button @click="resetFn()">Reset Name</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        //key untuk parrent
-        //validasi
-        props: {
-            //hanya menerima inputan string jika mau byk simpan diarray
-            myName: String,            
-            required: true, //harus diisi
-            default: 'Max' //defauld nilai
-        },
-        methods: {
-            switchName(){ //
-                return this.myName.split("").reverse().join("");
-            },
-            resetName(){ //reset name
-                this.myName = 'Max';
-                //emit(nama_listener, datas)
-                //fungsi utk komunikasi / kirim data ke parent => User.vue  dari child UserDetail.vue
-                this.$emit('nameWasReset', this.myName);
-            }
-        },
+export default {
+  //key untuk parrent
+  //validasi
+  props: {
+    myName: {
+      //hanya menerima inputan string jika mau byk simpan diarray
+      myName: String,
+      required: true, //harus diisi
+      default: "Max" //defauld nilai
+    },
+    //tangkap fungsi resetFn kiriman dari parent => User.vue 
+     resetFn: Function
+  },
+  methods: {
+    switchName() {
+      //
+      return this.myName
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName() {
+      //reset name
+      this.myName = "Max";
+      //emit(nama_listener, datas)
+      //fungsi utk komunikasi / kirim data ke parent => User.vue  dari child UserDetail.vue
+      this.$emit("nameWasReset", this.myName);
     }
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
