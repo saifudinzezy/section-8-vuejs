e598fe39e313b4e2aeacfb5628edaf0eef2c2823
saifudin <saifudin.zezy7@gmail.com>
2019-09-09 13:39:58 +0700

    probs

---
+++ src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <!-- name pertama dari child => UserDatail.vue -->
+                <!-- name kedua dari parent => User.vue -->
+                <app-user-detail :myName="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data() {
+            return {
+                //variabel name
+                name: 'Max'
+            }
+        },
+        methods: {
+            changeName(){
+                //ubah nilai name
+                this.name = "Anna"; //jgn lupa titik koma
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <!-- pangil key utk mengubah nilai name yg nantinya akan dipanggil oleh parent -->
+        <p>User name: {{ switchName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        //key untuk parrent
+        props: ['myName'],
+        methods: {
+            switchName(){
+                return this.myName.split("").reverse().join("");
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
